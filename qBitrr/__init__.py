0a3ff2da674076630e03a73e7f7782fbfa673697
Draper <27962761+Drapersniper@users.noreply.github.com>
2021-12-20 22:02:17 +0000

    [patch] Hotfix - Remove ujson support for any python implementation that is not CPython due to the SystemError crash that occurred on PyPy

--- qBitrr/__init__.py
+++ qBitrr/__init__.py
@@ -1,9 +1,15 @@
+import platform
+
 import qBitrr.logger  # noqa
 
 try:
-    import requests
-    import ujson
+    if platform.python_implementation() == "CPython":
+        # Only replace complexjson on CPython
+        # On PyPy it shows a SystemError when attempting to
+        # decode the responses from qbittorrentapi
+        import requests
+        import ujson
 
-    requests.models.complexjson = ujson
+        requests.models.complexjson = ujson
 except ImportError:
     pass
