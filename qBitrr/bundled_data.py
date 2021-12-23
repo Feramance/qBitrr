746a769d44fc3ed8256adc3ca0f4a31599b07706
Draper <27962761+Drapersniper@users.noreply.github.com>
2021-12-23 03:05:37 +0000

    [patch] Temp fix for https://github.com/Drapersniper/Qbitrr/issues/19#issuecomment-999970944

--- qBitrr/bundled_data.py
+++ qBitrr/bundled_data.py
@@ -3,17 +3,17 @@ import pathlib
 import sys
 
 
-def resource_path(relative_path: str) -> str:
-    base_path = getattr(
-        sys, "_MEIPASS", str(pathlib.Path(os.path.abspath(__file__)).parent.parent)
-    )
-    return os.path.join(base_path, relative_path)
+def get_git_hash() -> str:
+    base_path = getattr(sys, "_MEIPASS", None)
+    if base_path is not None:
+        return pathlib.Path(os.path.join(base_path, "git_hash.txt")).read_text().strip()  # FIXME
+    return "Non-Binary"
 
 
 version = "2.1.13"
-git_hash = pathlib.Path(resource_path("git_hash.txt")).read_text().strip()
+git_hash = get_git_hash()
+
 license_text = (
-    f"{pathlib.Path(resource_path('LICENSE')).read_text().strip()}\n\n"
-    "https://github.com/Drapersniper/Qbitrr/blob/master/LICENSE"
+    "Licence can be found on:\n\nhttps://github.com/Drapersniper/Qbitrr/blob/master/LICENSE"
 )
 patched_version = f"{version}-{git_hash}"
