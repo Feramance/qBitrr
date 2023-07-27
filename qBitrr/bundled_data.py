d90e35732bc9a0ae6e19a9e8690b54c65c229cb4
Feramance <techagius2093@gmail.com>
2023-07-27 16:13:27 +0200

    [skip ci] Update Release Hash for v2.7.3

--- qBitrr/bundled_data.py
+++ qBitrr/bundled_data.py
@@ -1,5 +1,5 @@
-version = "2.7.0"
-git_hash = "95345b0"
+version = "2.7.3"
+git_hash = "bf1d933"
 license_text = (
     "Licence can be found on:\n\nhttps://github.com/Feramance/Qbitrr/blob/master/LICENSE"
 )
