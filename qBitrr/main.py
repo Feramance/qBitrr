bf57e65e035154db87e9397ffbf94d8e7eb9c081
Feramance <techagius2093@gmail.com>
2023-08-30 08:39:34 +0200

    [patch] Updated support for qbittorrent 4.5.5

--- qBitrr/main.py
+++ qBitrr/main.py
@@ -31,8 +31,8 @@ run_logs(logger)
 
 class qBitManager:
     min_supported_version = VersionClass("4.3.9")
-    soft_not_supported_supported_version = VersionClass("4.5")
-    max_supported_version = VersionClass("4.5.4")
+    soft_not_supported_supported_version = VersionClass("4.5.x")
+    max_supported_version = VersionClass("4.5.5")
     _head_less_mode = False
 
     def __init__(self):
