8122bc242a0f10d799da86b8f778f37361aefd09
Feramance <techagius2093@gmail.com>
2023-09-06 09:23:43 +0200

    [build] Log level debug

--- qBitrr/arss.py
+++ qBitrr/arss.py
@@ -102,6 +102,7 @@ class Arr:
         else:
             self.logger = logging.getLogger(f"qBitrr.{self._name}")
         run_logs(self.logger)
+        self.logger.trace("Log level %s", self._LOG_LEVEL)
         self.completed_folder = pathlib.Path(COMPLETED_DOWNLOAD_FOLDER).joinpath(self.category)
         if not self.completed_folder.exists() and not SEARCH_ONLY:
             try:
