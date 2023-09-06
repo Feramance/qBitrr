f90a83cf69ef02f452cc285dbfeb5d9316fe3125
Feramance <techagius2093@gmail.com>
2023-09-06 08:33:10 +0200

    [build] Fix for failed torrents

--- qBitrr/arss.py
+++ qBitrr/arss.py
@@ -882,48 +882,48 @@ class Arr:
     #             self.timed_ignore_cache.add(k)
     #         self.recheck.clear()
 
-    def _process_failed(self) -> None:
-        to_delete_all = self.delete.union(
-            self.missing_files_post_delete, self.downloads_with_bad_error_message_blocklist
-        )
-        if self.missing_files_post_delete or self.downloads_with_bad_error_message_blocklist:
-            delete_ = True
-        else:
-            delete_ = False
-        skip_blacklist = {
-            i.upper() for i in self.skip_blacklist.union(self.missing_files_post_delete)
-        }
-        if to_delete_all:
-            self.needs_cleanup = True
-            payload = self.process_entries(to_delete_all)
-            if payload:
-                for entry, hash_ in payload:
-                    self._process_failed_individual(
-                        hash_=hash_, entry=entry, skip_blacklist=skip_blacklist
-                    )
-        if self.remove_from_qbit or self.skip_blacklist or to_delete_all:
-            # Remove all bad torrents from the Client.
-            temp_to_delete = set()
-            if to_delete_all:
-                self.manager.qbit.torrents_delete(hashes=to_delete_all, delete_files=True)
-            if self.remove_from_qbit or self.skip_blacklist:
-                temp_to_delete = self.remove_from_qbit.union(self.skip_blacklist)
-                self.manager.qbit.torrents_delete(hashes=temp_to_delete, delete_files=True)
+    # def _process_failed(self) -> None:
+    #     to_delete_all = self.delete.union(
+    #         self.missing_files_post_delete, self.downloads_with_bad_error_message_blocklist
+    #     )
+    #     if self.missing_files_post_delete or self.downloads_with_bad_error_message_blocklist:
+    #         delete_ = True
+    #     else:
+    #         delete_ = False
+    #     skip_blacklist = {
+    #         i.upper() for i in self.skip_blacklist.union(self.missing_files_post_delete)
+    #     }
+    #     if to_delete_all:
+    #         self.needs_cleanup = True
+    #         payload = self.process_entries(to_delete_all)
+    #         if payload:
+    #             for entry, hash_ in payload:
+    #                 self._process_failed_individual(
+    #                     hash_=hash_, entry=entry, skip_blacklist=skip_blacklist
+    #                 )
+    #     if self.remove_from_qbit or self.skip_blacklist or to_delete_all:
+    #         # Remove all bad torrents from the Client.
+    #         temp_to_delete = set()
+    #         if to_delete_all:
+    #             self.manager.qbit.torrents_delete(hashes=to_delete_all, delete_files=True)
+    #         if self.remove_from_qbit or self.skip_blacklist:
+    #             temp_to_delete = self.remove_from_qbit.union(self.skip_blacklist)
+    #             self.manager.qbit.torrents_delete(hashes=temp_to_delete, delete_files=True)
 
-            to_delete_all = to_delete_all.union(temp_to_delete)
-            for h in to_delete_all:
-                self.cleaned_torrents.discard(h)
-                self.sent_to_scan_hashes.discard(h)
-                if h in self.manager.qbit_manager.name_cache:
-                    del self.manager.qbit_manager.name_cache[h]
-                if h in self.manager.qbit_manager.cache:
-                    del self.manager.qbit_manager.cache[h]
-        if delete_:
-            self.missing_files_post_delete.clear()
-            self.downloads_with_bad_error_message_blocklist.clear()
-        self.skip_blacklist.clear()
-        self.remove_from_qbit.clear()
-        self.delete.clear()
+    #         to_delete_all = to_delete_all.union(temp_to_delete)
+    #         for h in to_delete_all:
+    #             self.cleaned_torrents.discard(h)
+    #             self.sent_to_scan_hashes.discard(h)
+    #             if h in self.manager.qbit_manager.name_cache:
+    #                 del self.manager.qbit_manager.name_cache[h]
+    #             if h in self.manager.qbit_manager.cache:
+    #                 del self.manager.qbit_manager.cache[h]
+    #     if delete_:
+    #         self.missing_files_post_delete.clear()
+    #         self.downloads_with_bad_error_message_blocklist.clear()
+    #     self.skip_blacklist.clear()
+    #     self.remove_from_qbit.clear()
+    #     self.delete.clear()
 
     def _process_file_priority(self) -> None:
         # Set all files marked as "Do not download" to not download.
@@ -3823,37 +3823,37 @@ class PlaceHolderArr(Arr):
                 self.timed_ignore_cache.add(k)
             self.recheck.clear()
 
-    # def _process_failed(self):
-    #     if not (self.delete or self.skip_blacklist):
-    #         return
-    #     to_delete_all = self.delete
-    #     skip_blacklist = {i.upper() for i in self.skip_blacklist}
-    #     if to_delete_all:
-    #         for arr in self.manager.managed_objects.values():
-    #             payload = arr.process_entries(to_delete_all)
-    #             if payload:
-    #                 for entry, hash_ in payload:
-    #                     if hash_ in arr.cache:
-    #                         arr._process_failed_individual(
-    #                             hash_=hash_, entry=entry, skip_blacklist=skip_blacklist
-    #                         )
-    #     if self.remove_from_qbit or self.skip_blacklist or to_delete_all:
-    #         # Remove all bad torrents from the Client.
-    #         temp_to_delete = set()
-    #         if to_delete_all:
-    #             self.manager.qbit.torrents_delete(hashes=to_delete_all, delete_files=True)
-    #         if self.remove_from_qbit or self.skip_blacklist:
-    #             temp_to_delete = self.remove_from_qbit.union(self.skip_blacklist)
-    #             self.manager.qbit.torrents_delete(hashes=temp_to_delete, delete_files=True)
-    #         to_delete_all = to_delete_all.union(temp_to_delete)
-    #         for h in to_delete_all:
-    #             if h in self.manager.qbit_manager.name_cache:
-    #                 del self.manager.qbit_manager.name_cache[h]
-    #             if h in self.manager.qbit_manager.cache:
-    #                 del self.manager.qbit_manager.cache[h]
-    #     self.skip_blacklist.clear()
-    #     self.remove_from_qbit.clear()
-    #     self.delete.clear()
+    def _process_failed(self):
+        if not (self.delete or self.skip_blacklist):
+            return
+        to_delete_all = self.delete
+        skip_blacklist = {i.upper() for i in self.skip_blacklist}
+        if to_delete_all:
+            for arr in self.manager.managed_objects.values():
+                payload = arr.process_entries(to_delete_all)
+                if payload:
+                    for entry, hash_ in payload:
+                        if hash_ in arr.cache:
+                            arr._process_failed_individual(
+                                hash_=hash_, entry=entry, skip_blacklist=skip_blacklist
+                            )
+        if self.remove_from_qbit or self.skip_blacklist or to_delete_all:
+            # Remove all bad torrents from the Client.
+            temp_to_delete = set()
+            if to_delete_all:
+                self.manager.qbit.torrents_delete(hashes=to_delete_all, delete_files=True)
+            if self.remove_from_qbit or self.skip_blacklist:
+                temp_to_delete = self.remove_from_qbit.union(self.skip_blacklist)
+                self.manager.qbit.torrents_delete(hashes=temp_to_delete, delete_files=True)
+            to_delete_all = to_delete_all.union(temp_to_delete)
+            for h in to_delete_all:
+                if h in self.manager.qbit_manager.name_cache:
+                    del self.manager.qbit_manager.name_cache[h]
+                if h in self.manager.qbit_manager.cache:
+                    del self.manager.qbit_manager.cache[h]
+        self.skip_blacklist.clear()
+        self.remove_from_qbit.clear()
+        self.delete.clear()
 
     # def process(self):
     #     self._process_errored()
