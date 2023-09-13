e8b3a378ae40ea8dfc4badacc9ab816b5864eb64
Shaun Agius <techagius2093@gmail.com>
2023-09-13 13:55:46 +0000

    [skip ci]Automated version bump: v >> v3.7.5-beta-11

--- qBitrr/bundled_data.py
+++ qBitrr/bundled_data.py
@@ -1,4 +1,4 @@
-version = "3.7.5-beta-10"
+version = "3.7.5-beta-11"
 git_hash = "38fbb80"
 license_text = (
     "Licence can be found on:\n\nhttps://github.com/Feramance/qBitrr/blob/master/LICENSE"
