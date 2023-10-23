53d1a0195d5aa14ad34f1ea524061a62d5958e58
Feramance <techagius2093@gmail.com>
2023-10-23 21:48:25 +0200

    [patch] Loop restart fix if there are too many search commands of stalled or bad torrents

--- qBitrr/arss.py
+++ qBitrr/arss.py
@@ -3763,14 +3763,16 @@ class Arr:
                             if years.index(self.search_current_year) != years_count - 1:
                                 years_index += 1
                                 self.search_current_year = years[years_index]
-                            elif self.arr_db_query_commands_count() == 0:
+                            elif self.arr_db_query_commands_count() == 0 or datetime.now() > (
+                                (timer + loop_timer) * 4
+                            ):
                                 self.refresh_download_queue()
                                 self.force_grab()
                                 raise RestartLoopException
                         elif (
                             datetime.now() > (timer + loop_timer)
                             and self.arr_db_query_commands_count() == 0
-                        ):
+                        ) or datetime.now() > ((timer + loop_timer) * 4):
                             self.refresh_download_queue()
                             self.force_grab()
                             raise RestartLoopException
