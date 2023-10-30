bc94669436e69c5989a3b7d52b9c16033f899f0e
Shaun Agius <techagius2093@gmail.com>
2023-10-30 13:21:15 +0000

    [skip ci] Update Release Hash for v3.7.13-beta-13

--- qBitrr/bundled_data.py
+++ qBitrr/bundled_data.py
@@ -1,5 +1,5 @@
 version = "3.7.13-beta-13"
-git_hash = "373a49d"
+git_hash = "6115bfe"
 license_text = (
     "Licence can be found on:\n\nhttps://github.com/Feramance/qBitrr/blob/master/LICENSE"
 )
