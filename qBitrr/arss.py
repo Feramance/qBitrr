af480a6166947e7beaf9519bbdafb15fe9ed3229
Feramance <techagius2093@gmail.com>
2023-11-16 10:33:48 +0100

    [minor] Starting implementation of new feature

--- qBitrr/arss.py
+++ qBitrr/arss.py
@@ -3834,13 +3834,13 @@ class Arr:
                                 self.search_current_year = years[years_index]
                             elif (
                                 datetime.now() >= (timer + loop_timer)
-                            ) and self.arr_db_query_commands_count == 0:
+                            ) or self.arr_db_query_commands_count == 0:
                                 self.refresh_download_queue()
                                 self.force_grab()
                                 raise RestartLoopException
                         elif (
                             datetime.now() >= (timer + loop_timer)
-                        ) and self.arr_db_query_commands_count == 0:
+                        ) or self.arr_db_query_commands_count == 0:
                             self.refresh_download_queue()
                             self.force_grab()
                             raise RestartLoopException
