fb82ab8a6d61e2a9ad4fe38ffdff57af061f6f39
Feramance <techagius2093@gmail.com>
2023-11-21 12:17:00 +0100

    Removed restart loop on no commands found

--- qBitrr/arss.py
+++ qBitrr/arss.py
@@ -3846,15 +3846,11 @@ class Arr:
                             if years.index(self.search_current_year) != years_count - 1:
                                 years_index += 1
                                 self.search_current_year = years[years_index]
-                            elif (
-                                datetime.now() >= (timer + loop_timer)
-                            ) or self.arr_db_query_commands_count() == 0:
+                            elif datetime.now() >= (timer + loop_timer):
                                 self.refresh_download_queue()
                                 self.force_grab()
                                 raise RestartLoopException
-                        elif (
-                            datetime.now() >= (timer + loop_timer)
-                        ) or self.arr_db_query_commands_count() == 0:
+                        elif datetime.now() >= (timer + loop_timer):
                             self.refresh_download_queue()
                             self.force_grab()
                             raise RestartLoopException
