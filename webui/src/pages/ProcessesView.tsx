4355bf3a50505d521792cc39dd47b91b092a89a8
Feramance <techagius2093@gmail.com>
2025-10-26 19:54:29 +0100

    Process auto refresh

--- webui/src/pages/ProcessesView.tsx
+++ webui/src/pages/ProcessesView.tsx
@@ -43,7 +43,7 @@ export function ProcessesView({ active }: ProcessesViewProps): JSX.Element {
     () => {
       void load();
     },
-    active ? 1000 : null
+    active ? 5000 : null
   );
 
   const handleRestart = useCallback(
