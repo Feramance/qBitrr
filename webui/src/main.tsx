dd5ed806e893103928af47a9fb882cdf9538b48a
Feramance <techagius2093@gmail.com>
2025-10-30 11:09:34 +0100

    Added missing Mantine Provider

--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
 import "./styles.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </StrictMode>
 );
